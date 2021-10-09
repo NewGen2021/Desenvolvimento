83ff33bfb2a45562cbd8dad9a70727149140c641
NewGen <86695262+NewGen2021@users.noreply.github.com>
2021-10-09 18:01:49 -0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,2 @@
 #install dependencias
 sudo apt-get install python-is-python
-sudo apt-get update
-sudo apt-get install apache2
-sudo apt update
-sudo apt install mysql-server
-sudo apt-get install python3-pip
-pip install Django
-sudo apt-get install libmysqlclient-dev
-sudo pip3 install mysqlclient
-pip install django-crispy-forms
-pip install django-phone-field
